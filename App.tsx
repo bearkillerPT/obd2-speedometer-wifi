450c8966c4e1475af7dbb98d3e4cee2165668789
bearkillerPT <gilteixeira@ua.pt>
2023-07-07 13:33:04 +0100

    added base test

--- App.tsx
+++ App.tsx
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useRef } from 'react';
+import { Text } from 'react-native';
 
-export default function App() {
+const App = () => {
+  const ws = useRef<WebSocket | null>(null);
+
+  useEffect(() => {
+    ws.current = new WebSocket('ws://192.168.0.10:35000');
+
+    ws.current.onopen = () => {
+      console.log("ws opened");
+      if (ws.current)
+      ws.current.send('01 0D'); // Send command to request vehicle speed
+    };
+
+    ws.current.onmessage = (e) => {
+      // A message was received
+      console.log(e.data);
+    };
+
+    ws.current.onerror = (e) => {
+      // An error occurred
+      console.log(e.currentTarget);
+    };
+
+    ws.current.onclose = () => {
+      console.log("ws closed");
+    };
+
+    return () => {
+      // Cleanup function
+      if (ws.current)
+      ws.current.close();
+    };
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Text>OB2 Reader</Text>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
